327e81991675a8d63a4a0f37711eb50c400667e1
michdcode <michd@alumni.stanford.edu>
2017-04-13 16:12:14 -0700

    finished chaper one exercises, need to start challenges

--- Math_Python/chp_one_prac.py
+++ Math_Python/chp_one_prac.py
@@ -1,3 +1,5 @@
+from fractions import Fraction
+
 def factors(num):
     """Find the factors of a given number."""
 
@@ -22,3 +24,53 @@ def multiplication_table(num):
 
     user_number = input("Enter a number: ")
     multiplication_table(float(user_number))
+
+
+def fahrenheit_to_celsius():
+    """Converts fahrenheit to Celsius."""
+
+    fahrenheit = float(input('Please enter the degrees in fahrenheit: '))
+    celsius = (fahrenheit - 32) * Fraction(5, 9)
+    print('Temperature in Celsius is: {0:.2f}'.format(celsius))
+
+
+def celsius_to_fahrenheit():
+    """Converts Celsius to Fahrenheit."""
+
+    celsius = float(input('Please enter the degrees in celsius: '))
+    fahrenheit = (celsius * Fraction(9, 5)) + 32
+    print('Temperature in Fahrenheit is: {0:.2f}'.format(fahrenheit))
+
+
+def inches_to_meters():
+    """Converts inches to meters."""
+
+    inches = float(input('Please enter the number of inches: '))
+    meters = (inches * 2.54)/100
+    print('Distance in meters is: {0}'.format(meters))
+
+
+def miles_to_kilometers():
+    """Converts miles to kilometers."""
+
+    miles = float(input('Please enter the number of miles: '))
+    KM = (miles * 1.609)
+    print('Distance in kilometers is: {0}'.format(KM))
+
+
+def calc_quadratic_equ_root(a, b, c):
+    """Finds the roots in a quadratic equation"""
+
+    D = (b*b - 4*a*c)**0.5
+    x1 = (-b + D)/(2*a)
+    x2 = (-b - D)/(2*a)
+    print('x1: {0}'.format(x1))
+    print('x2: {0}'.format(x2))
+
+
+def gather_root_data():
+    a = input('Enter a: ')
+    b = input('Enter b: ')
+    c = input('Enter c: ')
+    calc_quadratic_equ_root(float(a), float(b), float(c))
+
