92a5359a0e386b7d80e7c8faffdaa08cf78a5cf5
michdcode <michdcode@users.noreply.github.com>
2017-10-02 16:03:43 -0700

    Add files via upload
    Additional graphing with pyplot

--- Math_Python/chp_two_prac.py
+++ Math_Python/chp_two_prac.py
@@ -1,12 +1,49 @@
-from pylab import plot, show
+from pylab import plot, legend, show, title, xlabel, ylabel, axis, savegif
+# Pylab is best to use when plotting inside of a shell
+import matplotlib.pyplot as plt
 
-# x_numbs = [1, 2, 3]
-# y_numbs = [2, 4, 6]
-# plot(x_numbs, y_numbs, marker='o')
-# show()
+
+x_numbs = [1, 2, 3]
+y_numbs = [2, 4, 6]
+plot(x_numbs, y_numbs, marker='o')
+show()
 
 nyc_temp = [53.9, 56.3, 56.4, 53.4, 54.5, 55.8, 56.8, 55.0, 55.3, 54.0, 56.7, 56.4, 57.3]
 years = range(2000, 2013)
-plot(years, nyc_temp, marker='+')
+# plot(years, nyc_temp, marker='+')
+plot(nyc_temp, marker='o')
+axis() #will show the axis
+axis(ymin=0) #will change y-axis minimum to 0
 show()
-# left off on page 38
+
+
+#plotting three objects on one graph, legend, axis labels, title,  
+nyc_temp_2000 = [31.3, 37.3, 47.2, 51.0, 63.5, 71.3, 72.3, 72.7, 66.0, 57.0, 45.3, 31.1]
+nyc_temp_2006 = [40.9, 35.7, 43.1, 55.7, 63.1, 71.0, 77.9, 75.8, 66.6, 56.2, 51.9, 43.6]
+nyc_temp_2012 = [37.3, 40.9, 50.9, 54.8, 65.1, 71.0, 78.8, 76.7, 68.8, 58.0, 43.9, 41.5]
+
+months = range(1, 13)
+plot(months, nyc_temp_2000, months, nyc_temp_2006, months, nyc_temp_2012)
+title('Average Monthly Temperature in NYC')
+xlabel('Month')
+ylabel('Temperature')
+legend([2000, 2006, 2012])
+show()
+
+# to save a graph as a .pdf (or png or svg):
+from pylab import plot, savefig
+x = [1, 2, 3]
+y = [2, 4, 6]
+plot(x, y)
+savefig('my_graph.pdf')
+
+
+# #pyplot examples
+
+def create_graph():
+    x_numbers = [1, 2, 3]
+    y_numbers = [2, 4, 6]
+    plt.plot(x_numbers, y_numbers)
+    plt.show()
+if __name__ == '__main__':
+    create_graph()
